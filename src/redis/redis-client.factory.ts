ee1fb6953948cbc51ba683a11977b549e8b6be19
Arvin Ramezani <arvin.ramezani6@gmail.com>
2023-10-29 17:54:12 +0330

    Add redis error handling inside 'src/redis/redis-client-factory.ts'.

--- src/redis/redis-client.factory.ts
+++ src/redis/redis-client.factory.ts
@@ -2,23 +2,41 @@ import { FactoryProvider } from '@nestjs/common';
 import { createClient } from 'redis';
 import { RedisClient, REDIS_CLIENT } from './redis-client.type';
 
-// Redis Database number will change based on test, development and production environments
-const redisDatabaseNumber = {
-  test: 0,
-  development: 1,
-  production: 2,
-};
-
-const REDIS_URL = `redis://${process.env?.REDIS_HOST}:${process.env
-  ?.REDIS_PORT}/${redisDatabaseNumber[process.env?.NODE_ENV]}`;
-
 export const redisClientFactory: FactoryProvider<Promise<RedisClient>> = {
   provide: REDIS_CLIENT,
   useFactory: async () => {
+    const url = validateAndCreateRedisUrl();
+
     const client = createClient({
-      url: REDIS_URL,
+      url,
     });
-    await client.connect();
-    return client;
+
+    try {
+      await client.connect();
+      return client;
+    } catch (error) {
+      throw new Error('Fail to connect to Redis Server');
+    }
   },
+};
+
+const validateAndCreateRedisUrl = () => {
+  // Redis Database number will change based on test,
+  // development and production environments
+  const redisDatabaseNumber = {
+    test: 0,
+    development: 1,
+    production: 2,
+  };
+
+  if (!process.env?.REDIS_HOST || !process.env?.REDIS_PORT) {
+    throw new Error(
+      'Please provide valid REDIS_HOST and REDIS_PORT environment variables',
+    );
+  }
+
+  const redisUrl = `redis://${process.env?.REDIS_HOST}:${process.env
+    ?.REDIS_PORT}/${redisDatabaseNumber[process.env?.NODE_ENV]}`;
+
+  return redisUrl;
 };
